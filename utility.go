fed9f4aa37e8add3f28a23d83e66101d87097235
Dan Stroot <dan.stroot@gmail.com>
2017-03-04 08:12:41 -0800

    License

--- utility.go
+++ utility.go
@@ -56,14 +56,14 @@ func Check(e error) {
 // http://stackoverflow.com/questions/8307478/go-how-to-find-out-element-position-in-slice
 // It's used to find the slice position of the process activity step.
 // Then we use that to calculate the next action.  Usage:
-//
-// xs := []int{2, 4, 6, 8}
-// ys := []string{"C", "B", "K", "A"}
-// fmt.Println(
-//     SliceIndex(len(xs), func(i int) bool { return xs[i] == 5 }),
-//     SliceIndex(len(xs), func(i int) bool { return xs[i] == 6 }),
-//     SliceIndex(len(ys), func(i int) bool { return ys[i] == "Z" }),
-//     SliceIndex(len(ys), func(i int) bool { return ys[i] == "A" }))
+//     xs := []int{2, 4, 6, 8}
+//     ys := []string{"C", "B", "K", "A"}
+//     fmt.Println(
+//         SliceIndex(len(xs), func(i int) bool { return xs[i] == 5 }),
+//         SliceIndex(len(xs), func(i int) bool { return xs[i] == 6 }),
+//         SliceIndex(len(ys), func(i int) bool { return ys[i] == "Z" }),
+//         SliceIndex(len(ys), func(i int) bool { return ys[i] == "A" })
+//     )
 func SliceIndex(limit int, predicate func(i int) bool) (int, error) {
 	for i := 0; i < limit; i++ {
 		if predicate(i) {
